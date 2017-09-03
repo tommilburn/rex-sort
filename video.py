fc00463e14edcf731dd41108d8bbab4a625bbc1a
Tom Milburn <thomasjosephmilburn@gmail.com>
2017-09-03 11:32:46 -0400

    displaying movie and brightness info

--- video.py
+++ video.py
@@ -1,26 +1,17 @@
 import sys
-
-
 import numpy as np
 import cv2
 print cv2.__version__
-
 print "hello world"
-import numpy as np
-import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('rex.avi')
 
-while(True):
-    # Capture frame-by-frame
+while(cap.isOpened()):
     ret, frame = cap.read()
-
-
-    # Display the resulting frame
-    cv2.imshow('frame',ret)
+    cv2.imshow('frame',frame)
+    print np.sum(frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
