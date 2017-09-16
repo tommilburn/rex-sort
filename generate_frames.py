7faca48bdd95d160fa539c0008082a5a2a1c0cd7
Tom Milburn <thomasjosephmilburn@gmail.com>
2017-09-16 16:20:20 -0400

    housekeeping, git on windows is scary and strange

--- generate_frames.py
+++ generate_frames.py
@@ -8,13 +8,10 @@ from datetime import datetime
 print cv2.__version__
 print "hello world"
 startTime = datetime.now()
-outputfolder = './frames'
-files = glob.glob('./frames/*')
 outcsv = open("out.csv", "w")
-for f in files:
-    os.remove(f)
 
 cap = cv2.VideoCapture('rex.avi')
+#cv2.imshow(cap)
 frames = {}
 frameNumber = 0
 while(cap.isOpened()):
@@ -24,15 +21,13 @@ while(cap.isOpened()):
     ret, frame = cap.read()
     if (type(frame) == type(None)):
         break
-#    cv2.imshow('frame',frame)
     sum = np.sum(frame)
 #    print frameNumber, sum
     frameNumber = frameNumber + 1
     s = 'frames/' + str(sum) + ' ' + str(frameNumber) + '.png'
-#    cv2.imwrite(s, frame)
+    cv2.imwrite(s, frame)
     frames[frameNumber] = sum;
-#    if cv2.waitKey(1) & 0xFF == ord('q'):
-#        breast = mydict.keys()
+
 for key, value in sorted(frames.iteritems(), key=lambda (k,v): (v,k)):
         outcsv.write( "%s, %s\n" % (key, value))
 
